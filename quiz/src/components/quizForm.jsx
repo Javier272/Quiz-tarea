c7ce78dcf17e7df106af5c31742f51ddd269b598
Javier272 <147571233+Javier272@users.noreply.github.com>
2026-03-10 10:13:10 -0600

    1

--- quiz/src/components/quizForm.jsx
+++ quiz/src/components/quizForm.jsx
@@ -59,7 +59,7 @@ function QuizForm() {
                     <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
-                    <option value="5">5</option>
+                    <option value="5">5 c</option>
                 </select>
             </label>
             <br />
